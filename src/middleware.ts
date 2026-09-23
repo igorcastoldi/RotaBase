498defc21ddd60f017b628d7e49c931d1b1de9f7
igorcastoldi <igorvargascastoldi254br@gmail.com>
2026-09-23 18:20:17 -0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { createServerClient } from '@supabase/ssr'
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
@@ -14,7 +14,7 @@ export async function middleware(request: NextRequest) {
         getAll() {
           return request.cookies.getAll()
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: { name: string; value: string; options?: CookieOptions }[]) {
           cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
           supabaseResponse = NextResponse.next({
             request,
@@ -27,7 +27,6 @@ export async function middleware(request: NextRequest) {
     }
   )
 
-  // ESSA LINHA FORÇA O SITE A CHECAR SEU LOGIN ANTES DE TROCAR DE TELA
   await supabase.auth.getUser()
 
   return supabaseResponse
